ea41afb9713474690d6ae22168a129f601fd56c3
youssef askar <youssefaskar111@outlook.com>
2024-06-23 13:49:57 +0300

    add comment factory

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class CommentFactory extends Factory
+{
+    protected $model = Comment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    #[\Override]
+    public function definition(): array
+    {
+        return [
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'title' => $this->faker->word(),
+            'post_id' => Post::factory(),
+            'user_id' => User::factory(),
+        ];
+    }
+}
