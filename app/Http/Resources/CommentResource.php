51a8867a6e85b32c8082ef6f6827cff7b247ee36
YoussefIbraheem <youssefm.muhammed@gmail.com>
2024-06-25 16:54:21 +0300

    larastan fix

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-/** @mixin \App\Models\Comment */
+/** @mixin Comment
+ * @property-read string $vote
+ * @mixin \App\Models\Vote
+ * */
 class CommentResource extends JsonResource
 {
     /**
